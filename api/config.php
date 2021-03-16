be98ec3b5621ea40147a68330e50aead6e9c94de
filipljoljic <filip.ljoljic@stu.ibu.edu.ba>
2021-03-16 19:57:59 +0100

    dao setup

---
+++ api/config.php
@@ -0,0 +1,12 @@
+<?php
+
+class Config {
+const DB_HOST = "localhost";
+const DB_USERNAME = "root";
+const DB_PASSWORD = "864950sa";
+const DB_SCHEME = "petshop";
+}
+
+
+
+ ?>
---
+++ api/dao/AccountDao.class.php
@@ -0,0 +1,14 @@
+<?php
+
+class AccountDao extends BaseDao{
+
+public function add_account($account){
+
+
+}
+
+  
+}
+
+
+ ?>
